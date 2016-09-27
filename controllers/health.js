0053764f2cb148f64b533f87d932676ee70d0b5d
Kevin Ludwig <kevin.j.ludwig.-nd@disney.com>
2016-09-27 14:32:19 -0700

    add health check

---
+++ controllers/health.js
@@ -0,0 +1,3 @@
+module.exports = function* () {
+    this.status = 200;
+};
--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.get('/', require('./controllers/index'));
+router.get('/health', require('./controllers/health'));
 router.get('/list/:id', require('./controllers/list'));
 router.get('/detail/:id', require('./controllers/detail'));
 router.get('/game/:id', require('./controllers/game'));
